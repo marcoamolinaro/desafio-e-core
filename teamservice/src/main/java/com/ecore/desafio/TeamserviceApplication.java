defa618c6dcd63a5763e6e33650e4b6934dce8ae
marcoamolinaro <molinaromarcoaurelio@gmail.com>
2023-03-02 15:12:57 -0300

    adjust

---
+++ teamservice/src/main/java/com/ecore/desafio/TeamserviceApplication.java
@@ -0,0 +1,13 @@
+package com.ecore.desafio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamserviceApplication.class, args);
+	}
+
+}
